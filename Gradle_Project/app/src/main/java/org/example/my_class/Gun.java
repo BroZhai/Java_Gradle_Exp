53c6f4a92c897b442a2a7017f2bc42b8378a8933
Working Tech <k2785233247@gmail.com>
2025-07-18 15:26:32 +0800

    噔噔咚, 出现严重build错误, 而且得知建议先去看Maven, 此项目等Maven搞完之后有时间再来, 当务之急是想办法成功导入第三方库

---
+++ Gradle_Project/app/src/main/java/org/example/my_class/Gun.java
@@ -0,0 +1,35 @@
+package org.example.my_class; // 在Gradle声明的项目中, 声明java包的路径时应从 src/main/java 下的 'org.xxx'开始声明
+
+import java.io.Serializable;
+
+// Gun类
+public class Gun implements Serializable{
+    private String gun_name;
+    private String ammo_type;
+    private Boolean handGun;
+    private transient Integer gun_id; // 这个transient关键字表示'该属性'将不会被序列化(出于保护目的)
+
+    public Gun(String gun_name, String ammo_type, Boolean is_handGun, int gun_id){
+      this.gun_name = gun_name;
+      this.ammo_type = ammo_type;
+      this.handGun = is_handGun; 
+      this.gun_id = gun_id;
+    }
+    
+    public String get_name(){
+      return this.gun_name;
+    }
+
+    public String get_ammo_type(){
+      return this.ammo_type;
+    }
+
+    public boolean is_handGun(){
+      return this.handGun;
+    }
+
+    public int get_gun_id(){
+      return this.gun_id;
+    }
+
+}
